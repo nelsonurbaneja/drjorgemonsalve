9a809b7573afc5e732b2d2ffade9c43393d099c2
Nelson <nelson@hotmil.com>
2020-03-02 11:13:08 -0400

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var showMenu = function showMenu(idToggle, idMenu) {
+  var toggle = document.getElementById(idToggle);
+  var menu = document.getElementById(idMenu);
+
+  if (toggle && menu) {
+    toggle.addEventListener('click', function (e) {
+      menu.classList.toggle('show');
+    });
+  }
+};
+
+showMenu('main-toggle', 'main-menu');
+
+var comments = function comments(id) {
+  var fbComments = "<div class = \"fb-comments\" data-href = \"".concat(location.href, "\" data-width = \"\" data-numposts = \"5\" > </div>");
+  var container = document.getElementById(id);
+  addEventListener('DOMContentLoaded', function () {
+    if (id) container.innerHTML = fbComments;
+  });
+};
+
+comments('container'); // FORMULARIO
+// const form = document.getElementById('formulario')
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     let nombre = e.target.nombre.value
+//     let correo = e.target.correo.value
+//     let telefono = e.target.nombre.value
+//     let asunto = e.target.asunto.value
+//     const person = {
+//         nombre : nombre,
+//         correo : correo,
+//         telefono : telefono,
+//         asunto : asunto
+//     }
+//     console.log(person)
+// });
